0918de41f0f90363154f617b954882a7fce1dc9e
Mahen Gandhi <mahen.gandhi@civilmachines.com>
2019-07-14 13:10:19 +0530

    Made flutter project

---
+++ android/app/src/main/java/in/chakhle/chakh_le_admin/MainActivity.java
@@ -0,0 +1,13 @@
+package in.chakhle.chakh_le_admin;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
